f1fe5f604e65ef7ea6144667c75eedb2b6031c82
carrycat <hauluntsia@gmail.com>
2016-11-28 22:37:08 -0500

    remove patterns from urls

--- report_system/urls.py
+++ report_system/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from report_system import views
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(r'^(?P<target_model>[-\w]+)/(?P<id>\d+)/$', views.CreateTicketView.as_view(),
         name="ticket_create_api"),
-)
+]
